051d15ec6032faf166d61d73bfb153cb9f96e8b6
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2024-08-19 22:35:55 +0900

    拡張メソッドの実装

---
+++ Practice.CSharp.Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice.CSharp.Shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// 統一したフォーマットで返す拡張メソッド
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
